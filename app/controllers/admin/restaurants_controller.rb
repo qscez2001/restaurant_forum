072c48f53e3116c5d2009eb87531329a464f3fb2
Yuyu <qscez2001@gmail.com>
2017-12-09 20:29:19 +0800

    add restaurant new and create action and views for admin

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -5,4 +5,28 @@ before_action :authenticate_admin
   def index
     @restaurants = Restaurant.all
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      flash[:notice] = "restaurant was successfully created"
+      redirect_to admin_restaurants_path
+    else
+      flash.now[:alert] = "restaurant was failed to create"
+      render :new
+    end
+  end
+
+
+  private
+
+
+  # 在讀取表單資料時，基於安全考量，必須在參數傳入時多做一層處理，術語叫做 Strong Parameters，Rails 4 以上的版本強制要求加入此程序。
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description)
+  end
 end
