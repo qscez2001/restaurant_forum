823314ac50bff2ce6988a3113937993ab96189d9
Yuyu <qscez2001@gmail.com>
2017-12-14 19:25:16 +0800

    link category and restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,7 @@
 class Restaurant < ApplicationRecord
   validates_presence_of :name
-
   mount_uploader :image, PhotoUploader
+  # 如果應用程式的邏輯允許「未分類」的 Restaurant 資料，
+  # 你就需要在 belongs_to 的宣告後，加上 optional: true 的設定
+  belongs_to :category, optional: true
 end
