44eb113821a1997f4c0164d0ebbe813652f83e18
Yuyu <qscez2001@gmail.com>
2017-12-14 21:10:54 +0800

    add category name on restaurant index page and rerun dev:fake

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
 
-  # 加上 dependent: :destroy，表示在刪除某筆 Category 資料時，
+  # 加上 , dependent: :destroy，表示在刪除某筆 Category 資料時，
   # 需要一併刪除所有關聯的 Restaurant 資料。
-  has_many :restaurants dependent: :destroy
+  has_many :restaurants, dependent: :destroy
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,5 +3,5 @@ class Restaurant < ApplicationRecord
   mount_uploader :image, PhotoUploader
   # 如果應用程式的邏輯允許「未分類」的 Restaurant 資料，
   # 你就需要在 belongs_to 的宣告後，加上 optional: true 的設定
-  belongs_to :category, optional: true
+  belongs_to :category
 end
