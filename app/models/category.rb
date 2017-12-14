7ec6f1240894558c6f938fe4f3a45139de23ac90
Yuyu <qscez2001@gmail.com>
2017-12-14 22:27:39 +0800

    add form on index page for creating new category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-
+  validates_presence_of :name
   # 加上 , dependent: :destroy，表示在刪除某筆 Category 資料時，
   # 需要一併刪除所有關聯的 Restaurant 資料。
   has_many :restaurants, dependent: :destroy
