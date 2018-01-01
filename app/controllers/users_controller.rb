f6f144a76bb1110aec74ca0f7b9a1f84ada7c3bb
Yuyu <qscez2001@gmail.com>
2018-01-01 16:54:23 +0800

    costomer divise register form add name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @commented_restaurants = @user.restaurants
+    @commented_restaurants = @user.restaurants.uniq
   end
 
   def edit
