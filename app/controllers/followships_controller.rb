6b0c23f78f5b3658d2f5b25b54b9d77b2e8a88b5
Yuyu <qscez2001@gmail.com>
2018-01-19 20:03:05 +0800

    validate uniqueness of following_id in user_id scope & add create action for following

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,2 +1,17 @@
 class FollowshipsController < ApplicationController
+  
+  def create
+    # 由於 params[:following_id] 會回傳一個數字，因此在指定參數時，需要使用 following_id 這個選項。
+
+    @followship = current_user.followships.build(following_id: params[:following_id])
+
+    if @followship.save
+      flash[:notice] = "Successfully followed"
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:alert] = @followship.errors.full_messages.to_sentence
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
 end
--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,5 +1,11 @@
 class Followship < ApplicationRecord
   belongs_to :user
   # 自訂外鍵名稱並使用 class_name 來指定對應的 Model使用
-  belongs_to :following, class name: "User"
+  belongs_to :following, class_name: "User"
+
+  # 限制「每個 User 只能追蹤另一個 User 一次」，從資料表的角度來描述，
+  # 就是「特定的 user_id 下，只能有一個 followings_id」
+  # https://rails.ruby.tw/active_record_validations.html#uniqueness
+
+  validates :following_id, uniqueness: { scope: :user_id }
 end
